81178c06395cbd364ca8300e0d5a918e618c193d
tslearn <tslearn@163.com>
2019-09-10 23:31:18 +0800

    change

--- src/client.test.ts
+++ src/client.test.ts
@@ -1,6 +1,6 @@
 import * as WebSocket from "ws";
 import {Deferred} from "./deferred";
-import {RPCClient, WebSocketNetClient} from "./client";
+import {WebSocketNetClient} from "./client";
 import {Logger} from "./logger";
 import {sleep} from "./utils";
 
@@ -13,11 +13,12 @@ async function runWebSocketServer(
   const wss: WebSocket.Server = new WebSocket.Server({ port: port });
   const logger: Logger = new Logger();
 
+
   wss.on("connection", async (ws: WebSocket) => {
     logger.info("connection onopen");
 
     ws.onmessage = (event: WebSocket.MessageEvent) => {
-      logger.info("connection onmessage " + event.data.toString());
+      logger.info("connection onmessage");
       ws.send(event.data);
     };
 
@@ -98,18 +99,63 @@ describe("WebSocketNetClient tests", () => {
       });
   });
 
-  test("client_dev", async () => {
-    await runWebSocketServer(22002, async () => {
-      const deferred: Deferred<any> = new Deferred<any>();
+  test("WebSocketNetClient_connect", async () => {
+    await runWebSocketServer(
+      31002,
+      async (_: WebSocket.Server, logger: Logger) => {
+        const deferred: Deferred<any> = new Deferred<any>();
 
-      const rpcClient: RPCClient = new RPCClient("ws://127.0.0.1:12345/ws");
-      rpcClient.open();
+        // ok
+        const client1: WebSocketNetClient = new WebSocketNetClient(logger);
+        await sleep(30);
+        expect(client1.connect("ws://127.0.0.1:31002"))
+          .toStrictEqual(true);
+        await sleep(30);
+        client1.disconnect();
 
-      await sleep(1000);
-      rpcClient.close();
+        // duplicate connect
+        const client2: WebSocketNetClient = new WebSocketNetClient(logger);
+        await sleep(30);
+        expect(client2.connect("ws://127.0.0.1:31002"))
+          .toStrictEqual(true);
+        expect(client2.connect("ws://127.0.0.1:31002"))
+          .toStrictEqual(false);
+        await sleep(30);
+        client2.disconnect();
 
-      deferred.doResolve(true);
-      return deferred.promise;
-    });
+        //  onOpen is called
+        //  onMessage is called
+        //  onClose is called
+        let testData: Uint8Array = new Uint8Array(10000);
+        for (let i: number = 0; i < testData.byteLength; i++) {
+          testData[i] = (i % 255) + 1;
+        }
+        const client3: WebSocketNetClient = new WebSocketNetClient(logger);
+        let onOpenCalled: boolean = false;
+        let onCloseCalled: boolean = false;
+        let onMessageData: Uint8Array = new Uint8Array(0);
+        client3.onOpen = () => {
+          onOpenCalled = true;
+        };
+        client3.onClose = () => {
+          onCloseCalled = true;
+        };
+        client3.onBinary = (data: Uint8Array) => {
+          onMessageData = data;
+        };
+        expect(client3.connect("ws://127.0.0.1:31002"))
+          .toStrictEqual(true);
+        await sleep(30);
+        client3.send(testData);
+        await sleep(30);
+        client3.disconnect();
+        await sleep(30);
+        expect(onOpenCalled).toStrictEqual(true);
+        expect(onCloseCalled).toStrictEqual(true);
+        expect(onMessageData).toStrictEqual(testData);
+
+        deferred.doResolve(true);
+        return deferred.promise;
+      });
   });
 });
