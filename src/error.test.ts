00a6870472ad22d14222f01e28988372872c07b5
tslearn <tslearn@163.com>
2020-08-12 15:34:31 +0800

    fast dev

--- src/error.test.ts
+++ src/error.test.ts
@@ -1,9 +1,107 @@
 import {RPCError} from "./error";
 
 describe("RPCError tests", () => {
-  test("RPCError_new", () => {
-    const v1: RPCError = new RPCError(3.5, "message", "debug");
-    expect(v1.getMessage()).toStrictEqual("message");
-    expect(v1.getDebug()).toStrictEqual("debug");
+  test("RPCError_Kind", () => {
+    expect(RPCError.KindNone).toStrictEqual(0);
+    expect(RPCError.KindProtocol).toStrictEqual(1);
+    expect(RPCError.KindTransport).toStrictEqual(2);
+    expect(RPCError.KindReply).toStrictEqual(3);
+    expect(RPCError.KindReplyPanic).toStrictEqual(4);
+    expect(RPCError.KindRuntimePanic).toStrictEqual(5);
+    expect(RPCError.KindKernelPanic).toStrictEqual(6);
+  });
+
+  test("RPCError_newProtocolError", () => {
+    expect(RPCError.newProtocolError("message")).toStrictEqual(
+      new RPCError(RPCError.KindProtocol, "message", ""),
+    );
+  });
+
+  test("RPCError_newTransportError", () => {
+    expect(RPCError.newTransportError("message")).toStrictEqual(
+      new RPCError(RPCError.KindTransport, "message", ""),
+    );
+  });
+
+  test("RPCError_newReplyError", () => {
+    expect(RPCError.newReplyError("message")).toStrictEqual(
+      new RPCError(RPCError.KindReply, "message", ""),
+    );
+  });
+
+  test("RPCError_newReplyPanic", () => {
+    expect(RPCError.newReplyPanic("message")).toStrictEqual(
+      new RPCError(RPCError.KindReplyPanic, "message", ""),
+    );
+  });
+
+  test("RPCError_newRuntimePanic", () => {
+    expect(RPCError.newRuntimePanic("message")).toStrictEqual(
+      new RPCError(RPCError.KindRuntimePanic, "message", ""),
+    );
+  });
+
+  test("RPCError_newKernelPanic", () => {
+    expect(RPCError.newKernelPanic("message")).toStrictEqual(
+      new RPCError(RPCError.KindKernelPanic, "message", ""),
+    );
+  });
+
+  test("RPCError_getKind", () => {
+    expect(RPCError.newKernelPanic("message").getKind()).toStrictEqual(
+      RPCError.KindKernelPanic,
+    );
+  });
+
+  test("RPCError_getMessage", () => {
+    expect(RPCError.newKernelPanic("message").getMessage()).toStrictEqual(
+      "message",
+    );
+  });
+
+  test("RPCError_getDebug", () => {
+    expect(
+      new RPCError(RPCError.KindReply, "message", "debug").getDebug(),
+    ).toStrictEqual(
+      "debug",
+    );
+  });
+
+  test("RPCError_addDebug", () => {
+    expect(
+      new RPCError(RPCError.KindReply, "message", null).addDebug("debug"),
+    ).toStrictEqual(
+      new RPCError(RPCError.KindReply, "message", "debug"),
+    );
+
+    expect(
+      new RPCError(RPCError.KindReply, "message", "").addDebug("debug"),
+    ).toStrictEqual(
+      new RPCError(RPCError.KindReply, "message", "debug"),
+    );
+
+    expect(
+      new RPCError(RPCError.KindReply, "message", "debug").addDebug("debug"),
+    ).toStrictEqual(
+      new RPCError(RPCError.KindReply, "message", "debug\ndebug"),
+    );
+
+    expect(
+      new RPCError(RPCError.KindReply, "message", null).addDebug(null),
+    ).toStrictEqual(
+      new RPCError(RPCError.KindReply, "message", null),
+    );
+
+    expect(
+      new RPCError(RPCError.KindReply, "message", "").addDebug(null),
+    ).toStrictEqual(
+      new RPCError(RPCError.KindReply, "message", ""),
+    );
+
+    expect(
+      new RPCError(RPCError.KindReply, "message", "debug").addDebug(null),
+    ).toStrictEqual(
+      new RPCError(RPCError.KindReply, "message", "debug"),
+    );
   });
 });
--- src/error.ts
+++ src/error.ts
@@ -1,66 +1,67 @@
-export enum RPCErrorKind {
-  None = 0,
-  Protocol = 1,
-  Transport = 2,
-  Reply = 3,
-  ReplyPanic = 4,
-  RuntimePanic = 5,
-  KernelPanic = 6,
-}
-
 export class RPCError {
+  public static readonly KindNone: number = 0;
+  public static readonly KindProtocol: number = 1;
+  public static readonly KindTransport: number = 2;
+  public static readonly KindReply: number = 3;
+  public static readonly KindReplyPanic: number = 4;
+  public static readonly KindRuntimePanic: number = 5;
+  public static readonly KindKernelPanic: number = 6;
+
   public static newProtocolError(message: string): RPCError {
-    return new RPCError(RPCErrorKind.Protocol, message, "");
+    return new RPCError(RPCError.KindProtocol, message, "");
   }
 
   public static newTransportError(message: string): RPCError {
-    return new RPCError(RPCErrorKind.Transport, message, "");
+    return new RPCError(RPCError.KindTransport, message, "");
   }
 
   public static newReplyError(message: string): RPCError {
-    return new RPCError(RPCErrorKind.Reply, message, "");
+    return new RPCError(RPCError.KindReply, message, "");
   }
 
   public static newReplyPanic(message: string): RPCError {
-    return new RPCError(RPCErrorKind.ReplyPanic, message, "");
+    return new RPCError(RPCError.KindReplyPanic, message, "");
   }
 
   public static newRuntimePanic(message: string): RPCError {
-    return new RPCError(RPCErrorKind.RuntimePanic, message, "");
+    return new RPCError(RPCError.KindRuntimePanic, message, "");
   }
 
   public static newKernelPanic(message: string): RPCError {
-    return new RPCError(RPCErrorKind.KernelPanic, message, "");
+    return new RPCError(RPCError.KindKernelPanic, message, "");
   }
 
-  private readonly kind: RPCErrorKind;
-  private readonly message: string;
-  private debug: string;
+  private readonly kind: number;
+  private readonly message: string | null;
+  private debug: string | null;
 
-  public constructor(kind: RPCErrorKind, message: string, debug: string) {
+  public constructor(
+    kind: number, message: string | null, debug: string | null) {
     this.kind = kind;
     this.message = message;
     this.debug = debug;
   }
 
-  public getKind(): RPCErrorKind {
+  public getKind(): number {
     return this.kind;
   }
 
-  public getMessage(): string {
+  public getMessage(): string | null {
     return this.message;
   }
 
-  public getDebug(): string {
+  public getDebug(): string | null {
     return this.debug;
   }
 
-  public addDebug(debug: string): RPCError {
-    if (!this.debug) {
-      this.debug = debug;
-    } else {
-      this.debug += "\n";
-      this.debug += debug;
+  public addDebug(debug: string | null): RPCError {
+    if (debug != null && debug != "") {
+      if (this.debug != null && this.debug != "") {
+        this.debug = debug;
+      } else {
+        this.debug += "\n";
+        this.debug += debug;
+      }
     }
 
     return this;
